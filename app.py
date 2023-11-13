6bea7f27405cfdff0357c862e1666e3c2783494f
YashMayekar <yashvmayekar@gmail.com>
2023-11-14 03:49:37 +0530

    changed the UI and fixed some bugs

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template, request, send_file
-from Main import resize_image
-
+from main import resize_image
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template("index.html")
-
 @app.route('/resize', methods=['POST'])
 def resize():
-    uploaded_file = request.files['image']
+    uploaded_file = request.files['Image']
     percentage = int(request.form['percentage'])
     ext = uploaded_file.filename.split(".")[-1]
     resized_image = resize_image(uploaded_file, percentage, ext)
@@ -18,10 +15,5 @@ def resize():
             as_attachment=True,
             mimetype=f"image/{ext}"
     )
-    print("upload file name "+uploaded_file.filename)
-    print("percentage "+percentage)
-    print("download name "+download_name)
-    print("ext "+ext)
-
 if __name__ == '__main__':
     app.run(debug=True)
